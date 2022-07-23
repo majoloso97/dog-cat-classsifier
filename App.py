6e9bbe1e364b128f2e2cd473b6a608d81a24f821
majoloso97 <majoloso97@gmail.com>
2022-07-23 17:29:24 -0600

    :zap: Added examples memoization

--- App.py
+++ App.py
@@ -19,6 +19,14 @@ def get_resources_data(csv_url):
     return model_url, example_names, example_urls
 
 
+@st.experimental_memo
+def memoize_examples(example_names, example_urls, model):
+    examples = {}
+    for i in range(len(example_names)):
+        examples[example_names[i]] = model.show_predict_image(example_urls[i], source_type = 'url')
+    return examples
+
+
 def app_globals():
     st.set_page_config('Dogs-cats classifier',layout='wide')
     st.title('Model for classifying dogs and cats pictures')
@@ -32,14 +40,13 @@ url_refs = 'https://github.com/majoloso97/dog-cat-classsifier/blob/main/URL%20re
 model_url, example_names, example_urls = get_resources_data(url_refs)
 
 model = initiate_model(model_url)
+examples = memoize_examples(example_names, example_urls, model)
 
 with st.expander('Examples'):
     A, B = st.columns([3,1])
 
     selected_image = A.selectbox('Choose an image to test the model:', example_names)
-    selected_url = example_urls[example_names.index(selected_image)]
-
-    prediction, plot = model.show_predict_image(selected_url, source_type='url')
+    prediction, plot = examples[selected_image]
 
     A.subheader('The selected image is' + prediction)
     B.subheader("Loaded image:")
